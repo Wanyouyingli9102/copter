a4bb5d347ecc84ae02f66d60f60d400abcea990a
zuolian11 <162583807+zuolian11@users.noreply.github.com>
2024-05-08 22:45:34 +0800

    22：45

--- ArduCopter/mode_hang.cpp
+++ ArduCopter/mode_hang.cpp
@@ -1,7 +1,6 @@
 #include "Copter.h"
 #include <GCS_MAVLink/GCS.h>
-#include <AP_HAL/AP_HAL.h> // 这里加入了头文件
-#include <AP_Math/AP_Math.h>
+#include <AP_AHRS/AP_AHRS.h> // 可以用传感器头文件
 
 /*
  * Init and run calls for hang flight mode
@@ -9,124 +8,89 @@
  * writer lqd lz
 */
 
-// althold_init - initialise althold controller
-bool ModeHang::init(bool ignore_checks)
-{
-    // initialise the vertical position controller
-    if (!pos_control->is_active_z()) {
-        pos_control->init_z_controller();
+class ModeHang : public Mode {
+public:
+    ModeHang(Copter &copter) : Mode(copter, Mode::Number::HANG) {}
+
+    bool init(bool ignore_checks) override;
+    void run() override;
+    void get_add_sensor_deg(float &roll_targ, float &pitch_targ);
+};
+
+bool ModeHang::init(bool ignore_checks) {
+    if (!copter.pos_control->is_active_z()) {
+        copter.pos_control->init_z_controller();
     }
-
-    // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
-    pos_control->set_correction_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
-
+    copter.pos_control->set_max_speed_accel_z(-copter.get_pilot_speed_dn(), copter.g.pilot_speed_up, copter.g.pilot_accel_z);
+    copter.pos_control->set_correction_speed_accel_z(-copter.get_pilot_speed_dn(), copter.g.pilot_speed_up, copter.g.pilot_accel_z);
     return true;
 }
-
-// get the deg of sensor in centi-degs and add to target
-void ModeHang::get_add_sensor_deg(float &roll_targ, float &pitch_targ)
-{
+void ModeHang::get_add_sensor_deg(float &roll_targ, float &pitch_targ) {
     if (copter.failsafe.radio || !copter.ap.rc_receiver_present) {
         roll_targ = 0.0;
         pitch_targ = 0.0;
         return;
     }
 
-    // 假设 sensors->get_roll() 和 sensors->get_pitch() 返回的是弧度值
-    float roll_sensor_deg = degrees(AP::ahal->sensors->get_roll());
-    float pitch_sensor_deg = degrees(AP::ahal->sensors->get_pitch());
+    // 使用AHRS获取当前飞机的姿态角度
+    const AP_AHRS &ahrs = AP::ahrs();
+    float roll_sensor_deg = degrees(ahrs.roll);
+    float pitch_sensor_deg = degrees(ahrs.pitch);
 
+    // 增加从传感器读取的角度到目标角度
     roll_targ += roll_sensor_deg;
     pitch_targ += pitch_sensor_deg;
 }
 
-// althold_run - runs the althold controller
-// should be called at 100hz or more
-void ModeHang::run()
-{
+void ModeHang::run() {
     // set vertical speed and acceleration limits
     pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
 
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
-    // gcs().send_text(MAV_SEVERITY_CRITICAL,"mode hang fly");  //地面站消息发送
-    // get pilot desired lean angles
+    // gcs().send_text(MAV_SEVERITY_CRITICAL, "Mode hang fly");  // 地面站消息发送
+
     float target_roll, target_pitch;
     get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, attitude_control->get_althold_lean_angle_max_cd());
 
-    // get pilot's desired yaw rate
     float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
-
-    // get pilot desired climb rate
     float target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());
     target_climb_rate = constrain_float(target_climb_rate, -get_pilot_speed_dn(), g.pilot_speed_up);
 
-    // Alt Hold State Machine Determination
     AltHoldModeState althold_state = get_alt_hold_state(target_climb_rate);
 
-    // Alt Hold State Machine
     switch (althold_state) {
-
-    case AltHold_MotorStopped:
-        attitude_control->reset_rate_controller_I_terms();
-        attitude_control->reset_yaw_target_and_rate(false);
-        pos_control->relax_z_controller(0.0f);   // forces throttle output to decay to zero
-        break;
-
-    case AltHold_Landed_Ground_Idle:
-        attitude_control->reset_yaw_target_and_rate();
-        FALLTHROUGH;
-
-    case AltHold_Landed_Pre_Takeoff:
-        attitude_control->reset_rate_controller_I_terms_smoothly();
-        pos_control->relax_z_controller(0.0f);   // forces throttle output to decay to zero
-        break;
-
-    case AltHold_Takeoff:
-        // initiate take-off
-        if (!takeoff.running()) {
-            takeoff.start(constrain_float(g.pilot_takeoff_alt,0.0f,1000.0f));
-        }
-
-        // get avoidance adjusted climb rate
-        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
-
-        // set position controller targets adjusted for pilot input
-        takeoff.do_pilot_takeoff(target_climb_rate);
-        break;
-
-    case AltHold_Flying:
-        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-
-#if AC_AVOID_ENABLED == ENABLED
-        // apply avoidance
-        copter.avoid.adjust_roll_pitch(target_roll, target_pitch, copter.aparm.angle_max);
-#endif
-
-        // get avoidance adjusted climb rate
-        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
-
-        // update the vertical offset based on the surface measurement
-        copter.surface_tracking.update_surface_offset();
-
-        // Send the commanded climb rate to the position controller
-        pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
-        break;
+        case AltHold_MotorStopped:
+            attitude_control->reset_rate_controller_I_terms();
+            attitude_control->reset_yaw_target_and_rate(false);
+            pos_control->relax_z_controller(0.0f);  // forces throttle output to decay to zero
+            break;
+        case AltHold_Landed_Ground_Idle:
+            attitude_control->reset_yaw_target_and_rate();
+            [[fallthrough]]; // C++17 attribute to indicate intentional fallthrough
+        case AltHold_Landed_Pre_Takeoff:
+            attitude_control->reset_rate_controller_I_terms_smoothly();
+            pos_control->relax_z_controller(0.0f);
+            break;
+        case AltHold_Takeoff:
+            if (!takeoff.running()) {
+                takeoff.start(constrain_float(g.pilot_takeoff_alt, 0.0f, 1000.0f));
+            }
+            target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+            takeoff.do_pilot_takeoff(target_climb_rate);
+            break;
+        case AltHold_Flying:
+            motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+            #if AC_AVOID_ENABLED == ENABLED
+            copter.avoid.adjust_roll_pitch(target_roll, target_pitch, copter.aparm.angle_max);
+            #endif
+            target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+            copter.surface_tracking.update_surface_offset();
+            pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
+            break;
     }
 
-    //for sensor
-    //in centi-degrees
-    // target_roll +=5;
-    // target_pitch +=5;
-
-    //add the deg from sensor
-    get_add_sensor_deg(target_roll,target_pitch);
-
-    // target_roll, target_pitch 作为目标姿态输入，默认遥控中位就是飞机水平
-    // call attitude controller 这是姿态，不是海拔！！！
+    get_add_sensor_deg(target_roll, target_pitch);
     attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-
-    // run the vertical position controller and set output throttle
     pos_control->update_z_controller();
 }
